443ddda3da1aa502e50e71ca7c1ebf6357805c83
fknipp <franz@qnipp.com>
2026-03-26 10:01:54 +0100

    Configure server

--- server.js
+++ server.js
@@ -5,33 +5,7 @@ import { createServer } from 'vite';
 const app = express();
 const port = 3010;
 
-// app.use(express.static('public'));
-
-app.get('/api', (req, res) => {
-  res.json({ random: Math.random() });
-});
-
-/*
-const viteServer = await createServer({
-  root: process.cwd(),
-  logLevel: 'error',
-  server: {
-    middlewareMode: true,
-    watch: {
-      // During tests we edit the files too fast and sometimes chokidar
-      // misses change events, so enforce polling for consistency
-      usePolling: true,
-      interval: 1000,
-    },
-  },
-});
-
-app.use(viteServer.middlewares);
-
-app.listen(port, () => {
-  console.log(`Listening on http://localhost:${port}`);
-});
-*/
+/* Add your server routes here */
 
 ViteExpress.listen(app, port, () =>
   console.log(`Example app listening at http://localhost:${port}`)
